66c3329081af9505b37071254f2eba0cd5361b8b
raja-moukhass <moraja858@gmail.com>
2020-05-17 09:05:14 +0000

    add files

---
+++ CODECHEF/33125221.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int     main()
+{
+    int n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        int m ,r;
+        cin>>m>>r;
+        m +=r;
+        cout<<m<<endl;
+    }
+
+}
